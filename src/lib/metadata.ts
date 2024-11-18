66c83ec50fbd826afd881bdbf879c3715958bebf
Amine Hmidani filali <hmidanifilaliamine@gmail.com>
2024-11-18 15:20:14 +0100

    change favicon

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -6,7 +6,7 @@ export function constructMetadata({
   title = 'Amine Hmidani Filali', // TODO: Add a custom title
   description = "Étudiant en MQL, passionné de développement, maîtrisant Java, NodeJS, React, recherchant un stage innovant.", // TODO: Add a custom description
   image = '/profilepicture.jpg', // TODO: Add a custom image
-  icons = '/avatar.svg', // TODO: Add a custom icon
+  icons = '/favicon.ico', // TODO: Add a custom icon
   noIndex = false
 }: {
   title?: string
@@ -23,15 +23,9 @@ export function constructMetadata({
       description,
       images: [image]
     },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description,
-      images: [image],
-      creator: '@chrislonzo'
-    },
+   
     icons,
-    metadataBase: new URL('https://www.chrislonzo.com'),
+    metadataBase: new URL('http://www.aminehmidanifilali.me'),
     ...(noIndex && {
       robots: {
         index: false,
